1545ead07c3747e90d227736c35d6480bc4ccf7a
lanou <lanou@wangjin.local>
2016-04-14 19:24:26 +0800

    测试

---
+++ text/text/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  text
+//
+//  Created by lanou on 16/4/14.
+//  Copyright © 2016年 lanou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
